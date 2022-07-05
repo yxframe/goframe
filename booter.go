6e64def071e50a9a6bc99a1d6c90fe0f346f5de7
guanjianchang <guanjianchang@dianchu.com>
2022-07-05 18:22:20 +0800

    [goframe]1. Update odb mod,

--- booter.go
+++ booter.go
@@ -34,7 +34,7 @@ func NewBooter() *Booter {
 	}
 }
 
-func (b *Booter) Boot(srv Server, cfg SrvCfg, bootCfg *BootCfg, registerCb func() error) error {
+func (b *Booter) Boot(srv Server, cfg SrvCfg, bootCfg *BootCfg, registerSuccCb func() error) error {
 	var err error = nil
 
 	// check params
@@ -67,7 +67,7 @@ func (b *Booter) Boot(srv Server, cfg SrvCfg, bootCfg *BootCfg, registerCb func(
 	}
 
 	// start
-	err = b.start(srv, buildCfg, registerCb)
+	err = b.start(srv, buildCfg, registerSuccCb)
 	return err
 }
 
@@ -128,7 +128,7 @@ func (b *Booter) loadCfg(srvCfg *SrvBuildCfg, bootCfg *BootCfg) error {
 	return nil
 }
 
-func (b *Booter) start(srv Server, cfg *SrvBuildCfg, registerCb func() error) error {
+func (b *Booter) start(srv Server, cfg *SrvBuildCfg, registerSuccCb func() error) error {
 	var err error = nil
 	defer b.ec.DeferThrow("start", &err)
 
@@ -150,8 +150,8 @@ func (b *Booter) start(srv Server, cfg *SrvBuildCfg, registerCb func() error) er
 		return err
 	}
 
-	if registerCb != nil {
-		err = registerCb()
+	if registerSuccCb != nil {
+		err = registerSuccCb()
 		if err != nil {
 			return err
 		}
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/yxlib/httpsrv v0.2.6
-	github.com/yxlib/odb v0.2.7
+	github.com/yxlib/odb v0.2.8
 	github.com/yxlib/p2pnet v0.2.11
 	github.com/yxlib/reg v0.2.11
 	github.com/yxlib/rpc v0.2.4
