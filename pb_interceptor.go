7b1f5fe0b021cc440dfb4b246fb36134342cf79d
guanjianchang <guanjianchang@dianchu.com>
2022-11-02 15:10:26 +0800

    [goframe] 1. fix pb interceptor, new an object instead of reuse the proto object. 2. update server and httpsrv mod.

--- pb_interceptor.go
+++ pb_interceptor.go
@@ -20,6 +20,7 @@ type PbInterceptor struct {
 }
 
 func (i *PbInterceptor) OnPreHandle(req *server.Request, resp *server.Response) (int32, error) {
+	// request
 	if req.Payload != nil {
 		reqData, err := server.ProtoBinder.GetRequest(req.Mod, req.Cmd)
 		if err != nil {
@@ -31,6 +32,10 @@ func (i *PbInterceptor) OnPreHandle(req *server.Request, resp *server.Response) 
 			return server.RESP_CODE_NOT_SUPPORT_PROTO, ErrNotProtoType
 		}
 
+		// always new an object, because it do not reset after reuse.
+		msg := protoObj.ProtoReflect().New()
+		protoObj = msg.Interface()
+
 		err = proto.Unmarshal(req.Payload, protoObj)
 		if err != nil {
 			return server.RESP_CODE_UNMARSHAL_REQ_FAILED, err
@@ -39,12 +44,20 @@ func (i *PbInterceptor) OnPreHandle(req *server.Request, resp *server.Response) 
 		req.ExtData = protoObj
 	}
 
+	// response
 	respData, err := server.ProtoBinder.GetResponse(resp.Mod, resp.Cmd)
 	if err != nil {
 		return server.RESP_CODE_NOT_SUPPORT_PROTO, err
 	}
 
-	resp.ExtData = respData
+	protoObj, ok := respData.(protoreflect.ProtoMessage)
+	if !ok {
+		return server.RESP_CODE_NOT_SUPPORT_PROTO, ErrNotProtoType
+	}
+
+	// always new an object, because it do not reset after reuse.
+	msg := protoObj.ProtoReflect().New()
+	resp.ExtData = msg.Interface()
 	return 0, nil
 }
 
@@ -64,13 +77,13 @@ func (i *PbInterceptor) OnHandleCompletion(req *server.Request, resp *server.Res
 }
 
 func (i *PbInterceptor) OnResponseCompletion(req *server.Request, resp *server.Response) error {
-	if req != nil {
-		server.ProtoBinder.ReuseRequest(req.ExtData, req.Mod, req.Cmd)
-	}
+	// if req != nil {
+	// 	server.ProtoBinder.ReuseRequest(req.ExtData, req.Mod, req.Cmd)
+	// }
 
-	if resp != nil {
-		server.ProtoBinder.ReuseResponse(resp.ExtData, resp.Mod, resp.Cmd)
-	}
+	// if resp != nil {
+	// 	server.ProtoBinder.ReuseResponse(resp.ExtData, resp.Mod, resp.Cmd)
+	// }
 
 	return nil
 }
